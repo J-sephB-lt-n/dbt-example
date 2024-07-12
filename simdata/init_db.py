e4f966c992322fc02ecf36350c0ffd815671ca1b
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-07-12 21:31:41 +0200

    feat(source_data.yml-init_db.py): added source data freshness check

--- simdata/init_db.py
+++ simdata/init_db.py
@@ -1,4 +1,5 @@
 import argparse
+import datetime
 import duckdb
 import pathlib
 
@@ -14,40 +15,38 @@ def create_database(db_name: str) -> None:
 
     with duckdb.connect(f"databases/{db_name}") as con:
         con.execute(
-            """
+            f"""
     CREATE SCHEMA IF NOT EXISTS raw_source_data
     ;
 
     CREATE TABLE IF NOT EXISTS raw_source_data.users (
-        email_address STRING,
-        signup_datetime STRING
-    );
-
-    CREATE TABLE IF NOT EXISTS raw_source_data.cards (
-        email_address STRING,
-        card_num STRING
+            _data_loaded_at_utc STRING
+        ,   email_address STRING
+        ,   signup_datetime STRING
     );
 
     CREATE TABLE IF NOT EXISTS raw_source_data.orders (
-            order_id STRING
+            _data_loaded_at_utc STRING
+        ,   order_id STRING
         ,   user_email_address STRING
         ,   amount STRING
         ,   order_datetime STRING
     );
 
     CREATE TABLE IF NOT EXISTS raw_source_data.bank (
-            order_id STRING
+            _data_loaded_at_utc STRING
+        ,   order_id STRING
         ,   status STRING
     );
-    INSERT INTO raw_source_data.users(email_address, signup_datetime)
+    INSERT INTO raw_source_data.users(_data_loaded_at_utc, email_address, signup_datetime)
     VALUES 
-            ('a.person@email.com', '2069-07-24 17:05:55')
-        ,   ('some.ne@el.se', '2420-12-24 13:04:15')
-        ,   ('email@dre.ss', '2020-03-14 18:27:01')
+            ('{(datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(hours=-30)).strftime("%Y-%m-%d %H:%M:%S")}', 'a.person@email.com', '2069-07-24 17:05:55')
+        ,   ('{(datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(hours=-30)).strftime("%Y-%m-%d %H:%M:%S")}', 'some.ne@el.se', '2420-12-24 13:04:15')
+        ,   ('{(datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(hours=-30)).strftime("%Y-%m-%d %H:%M:%S")}', 'email@dre.ss', '2020-03-14 18:27:01')
     ;
-    INSERT INTO raw_source_data.orders(order_id, user_email_address, amount, order_datetime)
-    VALUES  ('51469410', 'some.ne@el.se', '800.85', '2025-10-12 06:01:44')
-        ,   ('51469411', 'email@dre.ss', '27.95', '2025-10-12 06:05:39')
+    INSERT INTO raw_source_data.orders(_data_loaded_at_utc, order_id, user_email_address, amount, order_datetime)
+    VALUES  ('{(datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(hours=-30)).strftime("%Y-%m-%d %H:%M:%S")}', '51469410', 'some.ne@el.se', '800.85', '2025-10-12 06:01:44')
+        ,   ('{(datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(hours=-30)).strftime("%Y-%m-%d %H:%M:%S")}', '51469411', 'email@dre.ss', '27.95', '2025-10-12 06:05:39')
     ;
     """
         )
